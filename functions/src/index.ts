fc13566533287be8b492d02073f0eedbd7237e68
scriptSQD <73490413+scriptSQD@users.noreply.github.com>
2022-05-03 22:36:00 +0300

    [M] - Edit menifest to include PWA description. [M] - Provide apple-touch-icon. [I] - Make cloud functions work (they need to be in us-central1 region to work with Firebase Hosting). [M] - Update dependencies.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
 import * as functions from "firebase-functions";
 
-const universal = require(`${process.cwd()}/dist/scriptsqd/server/main`).app;
+/* eslint-disable-next-line */
+const universal = require("../dist/scriptsqd/server/main").app;
+
 export const ssr = functions
     .runWith({
         memory: "128MB",
-        timeoutSeconds: 90,
+        timeoutSeconds: 15,
     })
-    .region("europe-central2")
     .https.onRequest(universal());
